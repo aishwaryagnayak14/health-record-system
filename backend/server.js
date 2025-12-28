f42b34929928a3fd0aaed678b5c508749f90a2a5
Aishwary <aishwaryagnayak14@gmail.com>
2025-12-28 13:09:13 +0530

    Update server.js for Replit deployment

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,16 @@
 const express = require("express");
 const cors = require("cors");
+const path = require("path");
 const app = express();
-const PORT = 5000;
+
+
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
+
+
+app.use(express.static(path.join(__dirname, "frontend"))); 
 
 const authRoutes = require("./routes/auth");
 const adminRoutes = require("./routes/admin");
@@ -15,5 +21,10 @@ app.use("/auth", authRoutes);
 app.use("/admin", adminRoutes);
 app.use("/doctor", doctorRoutes);
 app.use("/patient", patientRoutes);
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "frontend/index.html")); 
+});
+
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
